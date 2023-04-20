e6b9ec433000dfbc6427de51c94999fbafca7fb4
Valery Tropin <tropin.tropin@gmail.com>
2023-04-20 19:30:42 +0500

    Fix bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from environs import Env
+
+
+env: Env = Env()
+env.read_env('.env')
+
+bot: Bot = Bot(env('BOT_TOKEN'))
+dp: Dispatcher = Dispatcher()
+
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=r"Я ещё маленький бот, но скоро вырасту и научусь вам помогать. А пока предлагаю вам пройти сайт Пешеход Тура: https://peshehodtour.ru")
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
