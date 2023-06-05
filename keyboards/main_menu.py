b21bf335a67b70b8bde92ad64bf49bced27cfee6
Valery Tropin <tropin.tropin@gmail.com>
2023-06-05 22:33:19 +0300

    Add keyboards/main_menu.py

---
+++ keyboards/main_menu.py
@@ -0,0 +1,17 @@
+"""Here are the settings for the main menu of the bot"""
+
+from aiogram import Bot
+from aiogram.types import BotCommand
+
+from lexicon.lexicon import LEXICON_COMMANDS
+
+
+async def set_main_menu(bot: Bot):
+    main_menu_commands = [
+        BotCommand(
+            command=command,
+            description=description
+            ) for command, description in LEXICON_COMMANDS.items()
+    ]
+    await bot.set_my_commands(main_menu_commands)
+
