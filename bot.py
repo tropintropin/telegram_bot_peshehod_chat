b4640599f4b68051b4f24479dce931a3ea688c7a
Valery Tropin <tropin.tropin@gmail.com>
2023-06-14 16:20:59 +0300

    Fix bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
+from aiogram.filters import Text, Command
 
 from environs import Env
+
+from lexicon.greeting import greeting
+
 
 env: Env = Env()
 env.read_env('.env')
@@ -10,9 +14,16 @@ bot: Bot = Bot(token=env('BOT_TOKEN'), parse_mode='HTML')  # NB! This is token f
 dp: Dispatcher = Dispatcher()
 
 
+@dp.message(Command(commands=['start']))
+async def process_start_command(message: Message):
+    await message.answer(greeting)
+
+
 @dp.message()       # for all other messages
 async def send_echo(message: Message):
-    await message.reply(text="Для справки введите или нажмите /help")
+    await message.reply(text='''
+Для справки введите или нажмите /help
+Нажмите /start, чтобы вернуться в начало''')
 
 
 if __name__ == '__main__':
