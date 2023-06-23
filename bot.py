021e589c94107aff930d2748d3b76ef8fa002ae3
Valery Tropin <tropin.tropin@gmail.com>
2023-06-23 16:37:09 +0300

    Fix bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,18 @@ from lexicon.greeting import greeting, instruction
 from time import sleep
 
 
+async def main() -> None:
+    config: Config = load_config()
 
+    bot: Bot = Bot(token=config.tg_bot.token)
+    dp: Dispatcher = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
 
 
 # Работающий вариант бота:
