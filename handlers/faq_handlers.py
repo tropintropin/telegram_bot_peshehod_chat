cb9947c72f11bbd11b162e2d46818720cce9b043
Valery Tropin <tropin.tropin@gmail.com>
2023-06-23 13:23:47 +0300

    Fix handlers/faq_handlers.py

--- handlers/faq_handlers.py
+++ handlers/faq_handlers.py
@@ -1,3 +1,17 @@
 '''Here are the handlers for the FAQ Section.
 '''
 
+import json
+
+
+with open('../lexicon/faq.json', 'r', encoding='utf-8') as faq:
+    sections: dict = json.load(faq)['sections']
+
+
+with open('../lexicon/tours_list.json', 'r', encoding='utf-8') as tours_list:
+    tours: dict = json.load(tours_list)['tours']
+
+
+# print(sections['Оплата и бронирование'].keys())
+# print(tours)
+
