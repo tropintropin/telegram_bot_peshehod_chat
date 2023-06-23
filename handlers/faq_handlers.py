04b54232d94f420fcf5dde736306220add18f675
Valery Tropin <tropin.tropin@gmail.com>
2023-06-24 00:07:30 +0300

    Fix handlers/faq_handlers.py

--- handlers/faq_handlers.py
+++ handlers/faq_handlers.py
@@ -1,17 +1,31 @@
 '''Here are the handlers for the FAQ Section.
 '''
+from aiogram import Router
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
 
 import json
 
 
-with open('../lexicon/faq.json', 'r', encoding='utf-8') as faq:
+router: Router = Router()
+
+
+with open('lexicon/faq.json', 'r', encoding='utf-8') as faq:
     sections: dict = json.load(faq)['sections']
 
 
-with open('../lexicon/tours_list.json', 'r', encoding='utf-8') as tours_list:
+with open('lexicon/tours_list.json', 'r', encoding='utf-8') as tours_list:
     tours: dict = json.load(tours_list)['tours']
 
 
-# print(sections['Оплата и бронирование'].keys())
-# print(tours.keys())
+@router.message(Command(commands='tours'))
+async def process_tours_command(message: Message):
+    # TODO: Create keyboard from list
+    await message.answer(text='\n'.join(tours.keys()))
+
+
+@router.message(Command(commands='faq'))
+async def process_faq_command(message: Message):
+    # TODO: Create keyboard from list
+    await message.answer(text='\n'.join(sections.keys()))
 
