7fed53e2b5ef205c2e92f6664ec35559efb21674
Valery Tropin <tropin.tropin@gmail.com>
2023-06-23 16:28:15 +0300

    Fix bot.py

--- bot.py
+++ bot.py
@@ -1,39 +1,48 @@
+import asyncio
+
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Text, Command
 
 from environs import Env
 
+from config_data.config import Config, load_config
+
 from lexicon.greeting import greeting, instruction
 
 from time import sleep
 
 
-env: Env = Env()
-env.read_env('.env')
-
-bot: Bot = Bot(token=env('BOT_TOKEN'), parse_mode='HTML')  # NB! Change token for the prod bot!
-dp: Dispatcher = Dispatcher()
-
-
-@dp.message(Command(commands=['start']))
-async def process_start_command(message: Message):
-    # TODO: Добавить задержку между сообщениями
-    await message.answer(f'{greeting}')
-    sleep(2)
-    await message.answer(f'{instruction}')
-    sleep(2)
-    await message.answer('<pre><code>Наш бот находится в разработке, скоро здесь появится новый функционал 🤗</code></pre>')
 
 
 
-@dp.message()       # for all other messages
-async def send_echo(message: Message):
-    await message.reply(text='''
-Для справки введите или нажмите /help
-Нажмите /start, чтобы вернуться в начало''')
+# Работающий вариант бота:
+
+# env: Env = Env()
+# env.read_env('.env')
+
+# bot: Bot = Bot(token=env('BOT_TOKEN'), parse_mode='HTML')  # NB! Change token for the prod bot!
+# dp: Dispatcher = Dispatcher()
 
 
-if __name__ == '__main__':
-    dp.run_polling(bot)
+# @dp.message(Command(commands=['start']))
+# async def process_start_command(message: Message):
+#     # TODO: Добавить задержку между сообщениями
+#     await message.answer(f'{greeting}')
+#     sleep(2)
+#     await message.answer(f'{instruction}')
+#     sleep(2)
+#     await message.answer('<pre><code>Наш бот находится в разработке, скоро здесь появится новый функционал 🤗</code></pre>')
+
+
+
+# @dp.message()       # for all other messages
+# async def send_echo(message: Message):
+#     await message.reply(text='''
+# Для справки введите или нажмите /help
+# Нажмите /start, чтобы вернуться в начало''')
+
+
+# if __name__ == '__main__':
+#     dp.run_polling(bot)
 
