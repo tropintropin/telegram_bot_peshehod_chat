57e8b4ffb67d848c15274564527010c44dba8910
Valery Tropin <tropin.tropin@gmail.com>
2023-06-24 23:39:22 +0300

    Fix keyboards/list_keyboard.py

--- keyboards/list_keyboard.py
+++ keyboards/list_keyboard.py
@@ -7,5 +7,26 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 # Добавляем массивы нужной конфигурации с кнопками в основной массив клавиатуры
 
 
+def create_inline_kb(width: int,
+                     *args: str,
+                     **kwargs: str) -> InlineKeyboardMarkup:
+    kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    buttons: list[InlineKeyboardButton] = []
 
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=button,
+                callback_data=1  # TODO: Write short callback data in jsons
+            ))
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(
+                text=text,
+                callback_data=button
+            ))
+
+    kb_builder.row(*buttons, width=width)
+
+    return kb_builder.as_markup()
 
