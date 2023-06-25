30c1590d55c323df3a3835e4131890ce99683b69
Valery Tropin <tropin.tropin@gmail.com>
2023-06-25 17:00:42 +0300

    Fix handlers/faq_handlers.py

--- handlers/faq_handlers.py
+++ handlers/faq_handlers.py
@@ -4,31 +4,24 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
-from keyboards.list_keyboard import create_inline_kb
-
-import json
+from keyboards.list_keyboard import create_tours_inline_kb
+from services.services import get_faq_sections, get_tours_list
 
 
 router: Router = Router()
 
 
-with open('lexicon/faq.json', 'r', encoding='utf-8') as faq:
-    sections: dict = json.load(faq)['sections']
-
-
-with open('lexicon/tours_list.json', 'r', encoding='utf-8') as tours_list:
-    tours: dict = json.load(tours_list)['tours']
-
-
 @router.message(Command(commands='tours'))
 async def process_tours_command(message: Message):
-    keyboard = create_inline_kb(1, *tours.keys())
+    tours = get_tours_list()
+    keyboard = create_tours_inline_kb(1, tours, *tours.keys())
     await message.answer(text='Список всех туров:', reply_markup=keyboard)
     # TODO: Create keyboard from list
 
 
 @router.message(Command(commands='faq'))
 async def process_faq_command(message: Message):
+    sections = get_faq_sections()
     # TODO: Create keyboard from list
     await message.answer(text='\n\n'.join(sections.keys()))
 
