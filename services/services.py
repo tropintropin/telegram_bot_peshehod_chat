d5673723cb3ddf45c7ac8d2b8999025fcb3dd7b4
Valery Tropin <tropin.tropin@gmail.com>
2023-06-27 13:04:06 +0300

    Fix  services/services.py
    Add get_tour_specs() function

--- services/services.py
+++ services/services.py
@@ -13,5 +13,11 @@ def get_tours_list() -> dict[str, dict[str, str]]:
     return tours
 
 
+def get_tour_specs(callback: str) -> dict:
+    tour_specs: dict = get_tours_list()[callback]
+    return tour_specs
+
+
 # print(get_faq_sections())
 # print(get_tours_list())
+# print(get_tour_specs('vr_petra_ochami'))
