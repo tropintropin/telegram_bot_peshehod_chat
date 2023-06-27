46c22b485f627ea87a10ea03e7101be441f9f7c4
Valery Tropin <tropin.tropin@gmail.com>
2023-06-27 13:03:34 +0300

    Fix  keyboards/array_keyboards.py
    Add create_tour_specs_inline_kb() function

--- keyboards/array_keyboards.py
+++ keyboards/array_keyboards.py
@@ -21,3 +21,18 @@ def create_tours_list_inline_kb(width: int, user_dict: dict[str, dict[str, str]]
 
     return kb_builder.as_markup()
 
+def create_tour_specs_inline_kb(width: int, user_dict: dict[str, str | dict]) -> InlineKeyboardMarkup:
+    kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    buttons: list[InlineKeyboardButton] = []
+
+    for name, description in user_dict.items():
+        if name not in ('Название', 'О чём экскурсия?'):
+            buttons.append(InlineKeyboardButton(
+                text=name,
+                callback_data='CALLBACK'  # TODO: Change callback to description's value
+            ))
+
+        kb_builder.row(*buttons, width=width)
+
+        return kb_builder.as_markup()
+    
