fdae5e7db7f9aef939794cf2772b04b445180338
Valery Tropin <tropin.tropin@gmail.com>
2023-06-27 15:07:40 +0300

    Fix  services/services.py
    Add get_group_tours_list(), get_private_tours_list() functions

--- services/services.py
+++ services/services.py
@@ -13,6 +13,24 @@ def get_tours_list() -> dict[str, dict[str, str]]:
     return tours
 
 
+def get_group_tours_list():
+    all_tours = get_tours_list()
+    group_tours = {}
+    for key, value in all_tours.items():
+        if value['is_group_tour']:
+            group_tours[key] = value
+    return group_tours
+
+
+def get_private_tours_list():
+    all_tours = get_tours_list()
+    private_tours = {}
+    for key, value in all_tours.items():
+        if not value['is_group_tour']:
+            private_tours[key] = value
+    return private_tours
+
+
 def get_tour_specs(callback: str) -> dict:
     tour_specs: dict = get_tours_list()[callback]
     return tour_specs
@@ -21,3 +39,5 @@ def get_tour_specs(callback: str) -> dict:
 # print(get_faq_sections())
 # print(get_tours_list())
 # print(get_tour_specs('vr_petra_ochami'))
+# print(get_group_tours_list())
+# print(get_private_tours_list())
