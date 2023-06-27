a9f26dc3356690ee9f919f862495523d96739e4a
Valery Tropin <tropin.tropin@gmail.com>
2023-06-28 00:33:27 +0300

    Fix handlers/faq_handlers.py
    Fix process_tour_specs_press() handler

--- handlers/faq_handlers.py
+++ handlers/faq_handlers.py
@@ -13,47 +13,71 @@ router: Router = Router()
 
 
 # TODO: Do correct filter!
-# @router.callback_query(Text(text='vr_petra_ochami'))
-# async def process_tour_specs_press(callback: CallbackQuery):
-#     tour_specs_keyboard = create_tour_specs_inline_kb(1, get_tour_specs(str(callback.data)))
-#     await callback.message.answer(text='Дополнительные данные по туру:', reply_markup=tour_specs_keyboard)
+@router.callback_query(Text(text='vr_petra_ochami'))
+async def process_tour_specs_press(callback: CallbackQuery):
+    tour_specs = get_tour_specs(callback.data)
+
+    await callback.message.answer(text=tour_specs['Название'])
+    await callback.message.answer(text=tour_specs['О чём экскурсия?'])
+
+    tour_specs_keyboard = create_tour_specs_inline_kb(1, tour_specs)
+    await callback.message.answer(
+        text='Дополнительные данные по туру:',
+        reply_markup=tour_specs_keyboard
+        )
 
 
 @router.callback_query(Text(text='tours'))
 async def process_tours_press(callback: CallbackQuery):
     tours_keyboard = create_tours_inline_kb()
-    await callback.message.answer(text=LEXICON_RU['tours'], reply_markup=tours_keyboard)
+    await callback.message.answer(
+        text=LEXICON_RU['tours'],
+        reply_markup=tours_keyboard
+        )
 
 
 @router.message(Command(commands='tours'))
 async def process_tours_command(message: Message):
     tours_keyboard = create_tours_inline_kb()
-    await message.answer(text=LEXICON_RU['tours'], reply_markup=tours_keyboard)
+    await message.answer(
+        text=LEXICON_RU['tours'],
+        reply_markup=tours_keyboard
+        )
 
 
 @router.callback_query(Text(text='group_tours'))
 async def process_group_tours_press(callback: CallbackQuery):
     group_tours_keyboard = create_tours_list_inline_kb(1, get_group_tours_list())
-    await callback.message.answer(text=LEXICON_RU['group_tours'], reply_markup=group_tours_keyboard)
+    await callback.message.answer(
+        text=LEXICON_RU['group_tours'],
+        reply_markup=group_tours_keyboard
+        )
 
 
 @router.callback_query(Text(text='private_tours'))
 async def process_private_tours_press(callback: CallbackQuery):
     private_tours_keyboard = create_tours_list_inline_kb(1, get_private_tours_list())
-    await callback.message.answer(text=LEXICON_RU['private_tours'], reply_markup=private_tours_keyboard)
+    await callback.message.answer(
+        text=LEXICON_RU['private_tours'],
+        reply_markup=private_tours_keyboard
+        )
 
 
 @router.callback_query(Text(text='faq'))
 async def process_faq_press(callback: CallbackQuery):
     sections = get_faq_sections()
     # TODO: Create keyboard from list
-    await callback.message.answer(text='\n\n'.join(sections.keys()))
+    await callback.message.answer(
+        text='\n\n'.join(sections.keys())
+        )
 
 @router.message(Command(commands='faq'))
 async def process_faq_command(message: Message):
     sections = get_faq_sections()
     # TODO: Create keyboard from list
-    await message.answer(text='\n\n'.join(sections.keys()))
+    await message.answer(
+        text='\n\n'.join(sections.keys())
+        )
 
 
 # В расписание всех групповых туров надо добавить фразу
