9e8d811684e9008d3295906a8c7450008964162b
Valery Tropin <tropin.tropin@gmail.com>
2023-06-28 15:01:14 +0300

    Fix services/services.py
    Add cut_tour_specs_for_keyboard() function
    Fix type annotations for variables

--- services/services.py
+++ services/services.py
@@ -1,5 +1,6 @@
 from aiogram.types import CallbackQuery
 
+import copy
 import json
 
 
@@ -16,8 +17,8 @@ def get_tours_list() -> dict[str, dict[str, str]]:
 
 
 def get_group_tours_list():
-    all_tours = get_tours_list()
-    group_tours = {}
+    all_tours: dict = get_tours_list()
+    group_tours: dict = {}
     for key, value in all_tours.items():
         if value['is_group_tour']:
             group_tours[key] = value
@@ -25,8 +26,8 @@ def get_group_tours_list():
 
 
 def get_private_tours_list():
-    all_tours = get_tours_list()
-    private_tours = {}
+    all_tours: dict = get_tours_list()
+    private_tours: dict = {}
     for key, value in all_tours.items():
         if not value['is_group_tour']:
             private_tours[key] = value
@@ -36,4 +37,11 @@ def get_private_tours_list():
 def get_tour_specs(callback: str | CallbackQuery) -> dict:
     tour_specs: dict = get_tours_list()[callback]
     return tour_specs
+
+
+def cut_tour_specs_for_keyboard(user_dict: dict) -> dict:
+    specs: dict = copy.deepcopy(user_dict)
+    for key in ['is_group_tour', 'Название', 'О чём экскурсия?']:
+        specs.pop(key, None)
+    return specs
 
