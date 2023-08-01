4114a125521e48dd37931cb16b4f0fa01e861a87
Valery Tropin <tropin.tropin@gmail.com>
2023-08-01 15:52:18 +0300

    Fix bot.py
    Add comments to the code.

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+"""The entry point for the bot.
+"""
+
 import asyncio
 
 from aiogram import Bot, Dispatcher
@@ -8,21 +11,26 @@ from keyboards.main_menu import set_main_menu
 
 
 async def main() -> None:
+    # Loads the bot configuration from a file
     config: Config = load_config()
 
+    # Bot and dispatcher initialization
     bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
     dp: Dispatcher = Dispatcher()
 
+    # Connecting handlers
     dp.include_router(user_handlers.router)
     dp.include_router(tours_handlers.router)
     dp.include_router(other_handlers.router)
 
+    # Setting up the main menu
     await set_main_menu(bot)
 
+    # Deleting outdated webhooks from the bot
     await bot.delete_webhook(drop_pending_updates=True)
+    # Bot startup
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
     asyncio.run(main())
-
