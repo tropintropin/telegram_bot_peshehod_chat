5290dabb2cf3989cbd19da81dcaf394ba412a29c
Valery Tropin <tropin.tropin@gmail.com>
2023-08-01 14:58:41 +0300

    Fix handlers/user_handlers.py
    Add comments to the code.

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,3 +1,6 @@
+"""Handlers for the basic commands of the bot.
+"""
+
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
@@ -11,6 +14,7 @@ from time import sleep
 router: Router = Router()
 
 
+# Handler for the command "/start"
 @router.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(f'{greeting}')
@@ -20,7 +24,7 @@ async def process_start_command(message: Message):
     await message.answer('<pre><code>Наш бот находится в разработке, скоро здесь появится новый функционал 🤗</code></pre>')
 
 
+# Handler for the command "/help"
 @router.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(text=LEXICON_RU['/help'])
-
