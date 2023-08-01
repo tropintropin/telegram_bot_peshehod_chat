205555210cca7a9c59f90ca8caaaaa4e6bd1383d
Valery Tropin <tropin.tropin@gmail.com>
2023-08-01 14:52:39 +0300

    Fix handlers/tours_handlers.py
    Add comments to the code.

--- handlers/tours_handlers.py
+++ handlers/tours_handlers.py
@@ -1,5 +1,6 @@
-'''Here are the handlers for the Tours Section.
+'''Handlers for the "Tours" section.
 '''
+
 from aiogram import F, Router
 from aiogram.filters import Command, Text
 from aiogram.types import Message, CallbackQuery
@@ -13,6 +14,7 @@ from services.services import get_tours_list, get_faq_sections, get_tour_specs, 
 router: Router = Router()
 
 
+# Handler for the "Tour Specification" button press
 @router.callback_query(TourSpecItemCallbackFactory.filter())
 async def process_tour_spec_item_press(callback: CallbackQuery,
                                 callback_data: TourSpecItemCallbackFactory):
@@ -21,6 +23,7 @@ async def process_tour_spec_item_press(callback: CallbackQuery,
     await callback.message.answer(text=spec[callback_data.item])
 
 
+# Handler for the "Tour" button press
 @router.callback_query(ToursCallbackFactory.filter())
 async def process_tour_specs_press(callback: CallbackQuery,
                                 callback_data: ToursCallbackFactory):
@@ -36,6 +39,7 @@ async def process_tour_specs_press(callback: CallbackQuery,
         )
 
 
+# Handler for the "Tours" button press
 @router.callback_query(Text(text='tours'))
 async def process_tours_press(callback: CallbackQuery):
     tours_keyboard = create_tours_inline_kb()
@@ -45,6 +49,7 @@ async def process_tours_press(callback: CallbackQuery):
         )
 
 
+# Handler for the command "/tours"
 @router.message(Command(commands='tours'))
 async def process_tours_command(message: Message):
     tours_keyboard = create_tours_inline_kb()
@@ -54,6 +59,7 @@ async def process_tours_command(message: Message):
         )
 
 
+# Handler for the "Group Tours" button press
 @router.callback_query(Text(text='group_tours'))
 async def process_group_tours_press(callback: CallbackQuery):
     group_tours_keyboard = create_tours_list_inline_kb(1, get_group_tours_list())
@@ -63,6 +69,7 @@ async def process_group_tours_press(callback: CallbackQuery):
         )
 
 
+# Handler for the "Individual Tours" button press
 @router.callback_query(Text(text='private_tours'))
 async def process_private_tours_press(callback: CallbackQuery):
     private_tours_keyboard = create_tours_list_inline_kb(1, get_private_tours_list())
@@ -72,6 +79,7 @@ async def process_private_tours_press(callback: CallbackQuery):
         )
 
 
+# Handler for the "FAQ" button press
 @router.callback_query(Text(text='faq'))
 async def process_faq_press(callback: CallbackQuery):
     sections = get_faq_sections()
@@ -80,6 +88,7 @@ async def process_faq_press(callback: CallbackQuery):
         text='\n\n'.join(sections.keys())
         )
 
+# Handler for the command "/faq"
 @router.message(Command(commands='faq'))
 async def process_faq_command(message: Message):
     sections = get_faq_sections()
@@ -87,9 +96,3 @@ async def process_faq_command(message: Message):
     await message.answer(
         text='\n\n'.join(sections.keys())
         )
-
-
-# В расписание всех групповых туров надо добавить фразу
-# "в графике возможны изменения, актуальное расписание на сайте",
-# ну или что-то такое
-
