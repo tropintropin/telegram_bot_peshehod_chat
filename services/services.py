bc626a5e9851247f754d687f4864e949768fdd16
Valery Tropin <tropin.tropin@gmail.com>
2023-08-02 10:47:51 +0300

    Fix services/services.py
    Change code comments for the functions to docstrings.

--- services/services.py
+++ services/services.py
@@ -8,22 +8,22 @@ import copy
 import json
 
 
-# Retrieves sections and questions from the faq.json
 def get_faq_sections() -> dict[str, dict[str, dict[str, str]]]:
+    """Retrieves sections and questions from the faq.json"""
     with open('lexicon/faq.json', 'r', encoding='utf-8') as f:
         sections: dict[str, dict[str, dict[str, str]]] = json.load(f)['sections']
     return sections
 
 
-# Retrieves the list of all tours from the tours_list.json
 def get_tours_list() -> dict[str, dict[str, str]]:
+    """Retrieves the list of all tours from the tours_list.json"""
     with open('lexicon/tours_list.json', 'r', encoding='utf-8') as f:
         tours: dict[str, dict[str, str]] = json.load(f)['tours']
     return tours
 
 
-# Retrieves the list of group tours from the overall list of all tours
 def get_group_tours_list():
+    """Retrieves the list of group tours from the overall list of all tours"""
     all_tours: dict = get_tours_list()
     group_tours: dict = {}
     for key, value in all_tours.items():
@@ -32,8 +32,8 @@ def get_group_tours_list():
     return group_tours
 
 
-# Retrieves the list of private tours from the overall list of all tours
 def get_private_tours_list():
+    """Retrieves the list of private tours from the overall list of all tours"""
     all_tours: dict = get_tours_list()
     private_tours: dict = {}
     for key, value in all_tours.items():
@@ -42,17 +42,16 @@ def get_private_tours_list():
     return private_tours
 
 
-# Retrieves the tour specifications by its name from the list of all tours
 def get_tour_specs(callback: str | CallbackQuery) -> dict:
+    """Retrieves the tour specifications by its name from the list of all tours"""
     tour_specs: dict = get_tours_list()[callback]
     return tour_specs
 
 
-# Processes the dictionary containing information about tour specifications
-# for use in an inline keyboard
 def cut_tour_specs_for_keyboard(user_dict: dict) -> dict:
+    """Processes the dictionary containing information about tour specifications
+    for use in an inline keyboard"""
     specs: dict = copy.deepcopy(user_dict)
     for key in ['is_group_tour', 'Название', 'О чём экскурсия?']:
         specs.pop(key, None)
     return specs
-
