3476c93537848421b00baaf18fa33205d52172ef
Valery Tropin <tropin.tropin@gmail.com>
2023-08-03 13:04:45 +0300

    Fix handlers/other_handlers.py
    Add docstring to the handler.

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -9,6 +9,15 @@ router: Router = Router()
 
 @router.message()
 async def send_echo(message: Message):
+    """
+    Handle the received message and send a copy back to the sender.
+
+    This function is an echo bot that sends a copy of the received message back to the sender.
+    It uses the 'send_copy' method of the 'message' object to send the same message.
+
+    :param message: The received message object to be echoed back.
+    :type message: aiogram.types.Message
+    """
     try:
         await message.send_copy(chat_id=message.chat.id)  # TODO: Delete this before relise!
     except:
