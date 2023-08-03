8512b31cb517b0bb35c516a31fa8e7e15ec8cb57
Valery Tropin <tropin.tropin@gmail.com>
2023-08-03 14:09:51 +0300

    Fix  handlers/tours_handlers.py
    Change code comments to the docstrings.

--- handlers/tours_handlers.py
+++ handlers/tours_handlers.py
@@ -14,19 +14,39 @@ from services.services import get_tours_list, get_faq_sections, get_tour_specs, 
 router: Router = Router()
 
 
-# Handler for the "Tour Specification" button press
 @router.callback_query(TourSpecItemCallbackFactory.filter())
 async def process_tour_spec_item_press(callback: CallbackQuery,
                                 callback_data: TourSpecItemCallbackFactory):
+    """
+    Handle the callback query when a tour specification item's button is pressed.
+
+    This function processes the callback query when a tour specification item's button is pressed.
+    It retrieves the tour specifications and sends the corresponding information as a response.
+
+    :param callback: The callback query object.
+    :type callback: aiogram.types.CallbackQuery
+    :param callback_data: The callback data containing the tour and the specific item.
+    :type callback_data: TourSpecItemCallbackFactory
+    """
     spec = get_tours_list()[callback_data.tours]
     await callback.message.answer(text=fr'<strong>{callback_data.item}</strong>')
     await callback.message.answer(text=spec[callback_data.item])
 
 
-# Handler for the "Tour" button press
 @router.callback_query(ToursCallbackFactory.filter())
 async def process_tour_specs_press(callback: CallbackQuery,
                                 callback_data: ToursCallbackFactory):
+    """
+    Handle the callback query when a tour button is pressed.
+
+    This function processes the callback query when a tour button is pressed.
+    It retrieves the tour specifications and sends the corresponding information as a response.
+
+    :param callback: The callback query object.
+    :type callback: aiogram.types.CallbackQuery
+    :param callback_data: The callback data containing the tour.
+    :type callback_data: ToursCallbackFactory
+    """
     tour_specs = get_tour_specs(callback_data.tours)
 
     await callback.message.answer(text=fr"<strong>{tour_specs['Название']}</strong>")
@@ -39,9 +59,17 @@ async def process_tour_specs_press(callback: CallbackQuery,
         )
 
 
-# Handler for the "Tours" button press
 @router.callback_query(Text(text='tours'))
 async def process_tours_press(callback: CallbackQuery):
+    """
+    Handle the callback query when the "Tours" button is pressed.
+
+    This function processes the callback query when the "Tours" button is pressed.
+    It sends the available tours using an inline keyboard.
+
+    :param callback: The callback query object.
+    :type callback: aiogram.types.CallbackQuery
+    """
     tours_keyboard = create_tours_inline_kb()
     await callback.message.answer(
         text=LEXICON_RU['tours'],
@@ -49,9 +77,17 @@ async def process_tours_press(callback: CallbackQuery):
         )
 
 
-# Handler for the command "/tours"
 @router.message(Command(commands='tours'))
 async def process_tours_command(message: Message):
+    """
+    Handle the command "/tours".
+
+    This function processes the command "/tours" and sends
+    the available tours using an inline keyboard.
+
+    :param message: The received message object.
+    :type message: aiogram.types.Message
+    """
     tours_keyboard = create_tours_inline_kb()
     await message.answer(
         text=LEXICON_RU['tours'],
@@ -59,9 +95,17 @@ async def process_tours_command(message: Message):
         )
 
 
-# Handler for the "Group Tours" button press
 @router.callback_query(Text(text='group_tours'))
 async def process_group_tours_press(callback: CallbackQuery):
+    """
+    Handle the callback query when the "Group Tours" button is pressed.
+
+    This function processes the callback query when the "Group Tours" button is pressed.
+    It sends the list of group tours using an inline keyboard.
+
+    :param callback: The callback query object.
+    :type callback: aiogram.types.CallbackQuery
+    """
     group_tours_keyboard = create_tours_list_inline_kb(1, get_group_tours_list())
     await callback.message.answer(
         text=LEXICON_RU['group_tours'],
@@ -69,9 +113,17 @@ async def process_group_tours_press(callback: CallbackQuery):
         )
 
 
-# Handler for the "Individual Tours" button press
 @router.callback_query(Text(text='private_tours'))
 async def process_private_tours_press(callback: CallbackQuery):
+    """
+    Handle the callback query when the "Private Tours" button is pressed.
+
+    This function processes the callback query when the "Private Tours" button is pressed.
+    It sends the list of private tours using an inline keyboard.
+
+    :param callback: The callback query object.
+    :type callback: aiogram.types.CallbackQuery
+    """
     private_tours_keyboard = create_tours_list_inline_kb(1, get_private_tours_list())
     await callback.message.answer(
         text=LEXICON_RU['private_tours'],
@@ -79,18 +131,34 @@ async def process_private_tours_press(callback: CallbackQuery):
         )
 
 
-# Handler for the "FAQ" button press
 @router.callback_query(Text(text='faq'))
 async def process_faq_press(callback: CallbackQuery):
+    """
+    Handle the callback query when the "FAQ" button is pressed.
+
+    This function processes the callback query when the "FAQ" button is pressed.
+    It sends the sections from the FAQ using an inline keyboard.
+
+    :param callback: The callback query object.
+    :type callback: aiogram.types.CallbackQuery
+    """
     sections = get_faq_sections()
     # TODO: Create keyboard from list
     await callback.message.answer(
         text='\n\n'.join(sections.keys())
         )
 
-# Handler for the command "/faq"
 @router.message(Command(commands='faq'))
 async def process_faq_command(message: Message):
+    """
+    Handle the command "/faq".
+
+    This function processes the command "/faq" and sends the sections
+    from the FAQ using an inline keyboard.
+
+    :param message: The received message object.
+    :type message: aiogram.types.Message
+    """
     sections = get_faq_sections()
     # TODO: Create keyboard from list
     await message.answer(
