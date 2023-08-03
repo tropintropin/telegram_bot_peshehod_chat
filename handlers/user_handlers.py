8d56517ce7f2e0bb9f52b1fe75dfcc1e42d419ff
Valery Tropin <tropin.tropin@gmail.com>
2023-08-03 14:16:15 +0300

    Fix  handlers/user_handlers.py
    Change code comments to the docstrings.

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -14,17 +14,33 @@ from lexicon.lexicon import LEXICON_RU
 router: Router = Router()
 
 
-# Handler for the command "/start"
 @router.message(CommandStart())
 async def process_start_command(message: Message):
+    """
+    Handle the command "/start".
+
+    This function processes the "/start" command when the user starts interacting with the bot.
+    It sends a greeting message, an instruction message, and a message about the bot being in development.
+
+    :param message: The received message object.
+    :type message: aiogram.types.Message
+    """
     await message.answer(f'{greeting}')
     await sleep(2)
     await message.answer(f'{instruction}')
     await sleep(2)
+    # TODO: Change the message & docstring for this answer when production:
     await message.answer('<pre><code>Наш бот находится в разработке, скоро здесь появится новый функционал 🤗</code></pre>')
 
 
-# Handler for the command "/help"
 @router.message(Command(commands='help'))
 async def process_help_command(message: Message):
+    """
+    Handle the command "/help".
+
+    This function processes the "/help" command and sends the help message to the user.
+
+    :param message: The received message object.
+    :type message: aiogram.types.Message
+    """
     await message.answer(text=LEXICON_RU['/help'])
