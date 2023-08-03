a6fcf82abb57b3de899dc9f9fc62177e5c1e13d2
Valery Tropin <tropin.tropin@gmail.com>
2023-08-03 14:30:04 +0300

    Fix  keyboards/inline_keyboards.py
    Fix docstrings in the functions.

--- keyboards/inline_keyboards.py
+++ keyboards/inline_keyboards.py
@@ -10,7 +10,15 @@ from services.services import cut_tour_specs_for_keyboard
 
 
 def create_tours_inline_kb() -> InlineKeyboardMarkup:
-    """Creates an inline keyboard with two buttons: 'Group Tours' and 'Individual Tours'"""
+    """
+    Create an inline keyboard with two buttons: 'Group Tours' and 'Private Tours'.
+
+    This function creates an inline keyboard with two buttons: 'Group Tours' and 'Private Tours'.
+    It returns the created keyboard with the buttons.
+
+    :return: The InlineKeyboardMarkup object with the 'Group Tours' and 'Private Tours' buttons.
+    :rtype: InlineKeyboardMarkup
+    """
     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
     buttons: list[InlineKeyboardButton] = [
         InlineKeyboardButton(text='Групповые туры', callback_data='group_tours'),
@@ -21,7 +29,19 @@ def create_tours_inline_kb() -> InlineKeyboardMarkup:
 
 
 def create_tours_list_inline_kb(width: int, user_dict: dict[str, dict[str, str]]) -> InlineKeyboardMarkup:
-    """Creates an inline keyboard with buttons for the list of tours"""
+    """
+    Create an inline keyboard with buttons for the list of tours.
+
+    This function creates an inline keyboard with buttons for each tour in the given 'user_dict'.
+    It returns the created keyboard with the tour buttons and a button to return to the list of tours.
+
+    :param width: The width of the keyboard (number of buttons in a row).
+    :type width: int
+    :param user_dict: The dictionary containing information about tours.
+    :type user_dict: dict[str, dict[str, str]]
+    :return: The InlineKeyboardMarkup object with tour buttons and a button to return to the list of tours.
+    :rtype: InlineKeyboardMarkup
+    """
     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
     buttons: list[InlineKeyboardButton] = []
 
@@ -43,7 +63,21 @@ def create_tours_list_inline_kb(width: int, user_dict: dict[str, dict[str, str]]
 
 
 def create_tour_specs_inline_kb(width: int, user_dict: dict[str, str | dict], tour) -> InlineKeyboardMarkup:
-    """Creates an inline keyboard with buttons for the list of tour specifications"""
+    """
+    Create an inline keyboard with buttons for the list of tour specifications.
+
+    This function creates an inline keyboard with buttons for each tour specification in the given 'user_dict'.
+    It returns the created keyboard with the tour specification buttons and a button to return to the list of tours.
+
+    :param width: The width of the keyboard (number of buttons in a row).
+    :type width: int
+    :param user_dict: The dictionary containing information about tour specifications.
+    :type user_dict: dict[str, str | dict]
+    :param tour: The name of the tour associated with the specifications.
+    :type tour: str
+    :return: The InlineKeyboardMarkup object with tour specification buttons and a button to return to the list of tours.
+    :rtype: InlineKeyboardMarkup
+    """
     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
     buttons: list[InlineKeyboardButton] = []
 
