bcc3b5f36c2b169da2a01f7167b10e5911f061d8
Valery Tropin <tropin.tropin@gmail.com>
2023-08-04 16:28:40 +0300

    Fix handlers/faq_handlers.py
    Add process_faq_specs_press() and process_faq_specs_item_press()
    handlers.

--- handlers/faq_handlers.py
+++ handlers/faq_handlers.py
@@ -5,10 +5,66 @@ from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.types import CallbackQuery, Message
 
-from keyboards.inline_keyboards import create_faq_section_list_inline_kb
-from services.services import get_faq_sections
+from config_data.config import FAQCallbackFactory, ItemsFAQCallbackFactory
+from keyboards.inline_keyboards import (create_faq_section_item_inline_kb,
+                                        create_faq_section_list_inline_kb)
+from services.services import cut_faq_section_items, get_faq_sections
+from handlers.tours_handlers import process_tours_press
+
 
 router: Router = Router()
+
+
+@router.callback_query(ItemsFAQCallbackFactory.filter())
+async def process_faq_specs_item_press(callback: CallbackQuery,
+                                        callback_data: ItemsFAQCallbackFactory):
+    if callback_data.item == 'tours':  # Move to Tours Section if true
+        await process_tours_press(callback)
+    else:
+        await callback.message.delete()
+
+        items_list = get_faq_sections()[callback_data.section]
+        list_name = items_list['section_name']
+        items_list = cut_faq_section_items(items_list)
+        items_list_keyboard = create_faq_section_item_inline_kb(1, items_list, callback_data.section)
+        await callback.message.answer(
+        text=fr"<strong>{list_name}</strong>",
+        reply_markup=items_list_keyboard
+        )
+
+        item_specs = get_faq_sections()[callback_data.section][callback_data.item]
+        await callback.message.answer(text=fr"<strong>{item_specs['question']}</strong>")
+        await callback.message.answer(text=fr"<strong>{item_specs['answer']}</strong>")
+
+        
+
+
+@router.callback_query(FAQCallbackFactory.filter())
+async def process_faq_specs_press(callback: CallbackQuery,
+                                    callback_data: FAQCallbackFactory):
+    sections = get_faq_sections()
+    faq_section_keyboard = create_faq_section_list_inline_kb(
+        width=1,
+        user_dict=sections
+        )
+    
+    section = sections[callback_data.section]
+    list_name = section['section_name']
+    items_list = cut_faq_section_items(section)
+    
+    items_list_keyboard = create_faq_section_item_inline_kb(1, items_list, callback_data.section)
+
+    await callback.message.delete()
+
+    await callback.message.answer(
+            text=r'<strong>ЧАВо (список частых вопросов и ответов):</strong>',
+            reply_markup=faq_section_keyboard
+            )
+
+    await callback.message.answer(
+        text=fr'<strong>{list_name}</strong>',
+        reply_markup=items_list_keyboard
+        )
 
 
 @router.callback_query(F.data == 'faq')
@@ -29,7 +85,7 @@ async def process_faq_press(callback: CallbackQuery):
         )
 
     await callback.message.answer(
-        text='Выберите тему:',
+        text=r'<strong>ЧАВо (список частых вопросов и ответов):</strong>',
         reply_markup=faq_section_keyboard
         )
 
@@ -52,6 +108,6 @@ async def process_faq_command(message: Message):
         )
 
     await message.answer(
-        text='Выберите тему:',
+        text=fr'<strong>ЧАВо (список частых вопросов и ответов):</strong>',
         reply_markup=faq_section_keyboard
         )
