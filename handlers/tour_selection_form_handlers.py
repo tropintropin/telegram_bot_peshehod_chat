8fea882d7a8341afc0ecb4cdf068dde2227712cd
Valery Tropin <tropin.tropin@gmail.com>
2023-11-03 15:25:19 +0300

    Fix handlers/tour_selection_form_handlers.py
    Add imports.

--- handlers/tour_selection_form_handlers.py
+++ handlers/tour_selection_form_handlers.py
@@ -1,3 +1,11 @@
-'''Here are the handlers for the Tour Selection Form Section.
+'''Handlers for the Tour Selection Form Section.
 '''
 
+from aiogram import F, Router
+from aiogram.filters import Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import default_state
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import CallbackQuery, Message
+
+from services.fsm import FSMTourSelection
