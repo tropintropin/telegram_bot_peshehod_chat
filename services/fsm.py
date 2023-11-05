c58e1572a565cbbd9847ba6449b979dffff6e0dd
Valery Tropin <tropin.tropin@gmail.com>
2023-11-06 00:29:16 +0300

    Fix services/fsm.py
    Add class annotation for FSM in Tour Selection Form Section.

--- services/fsm.py
+++ services/fsm.py
@@ -2,6 +2,16 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class FSMTourSelection(StatesGroup):
+    """
+    Finite State Machine for Tour Selection Form Section.
+
+    State Descriptions:
+    - is_group: Choice between group or individual tour.
+    - have_children: Presence of children in the tourist group.
+    - kids: Age group of accompanying children if any.
+    - visit: Indicates if it's the first city visit or not.
+    """
+
     is_group = State()
     have_children = State()
     kids = State()
