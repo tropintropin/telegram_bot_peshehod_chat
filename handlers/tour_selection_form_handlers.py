23dac23a162a030840d6cf466750e7b8f15af280
Valery Tropin <tropin.tropin@gmail.com>
2023-11-07 17:34:53 +0300

    Fix handlers/tour_selection_form_handlers.py

--- handlers/tour_selection_form_handlers.py
+++ handlers/tour_selection_form_handlers.py
@@ -5,7 +5,7 @@ from aiogram import F, Router
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import Redis, RedisStorage
 from aiogram.types import CallbackQuery, Message
 
 from services.fsm import FSMTourSelection
