2b701198827531317938154cafff4876b3aa9389
Valery Tropin <tropin.tropin@gmail.com>
2023-11-29 14:13:18 +0300

    Add services/SQLite_example.py + 2 .sql files
    Add a module for working with the SQLite database.

---
+++ services/SQLite_example.py
@@ -0,0 +1,53 @@
+import sqlite3
+from sqlite3 import Error
+from typing import Optional
+
+
+def create_connection(path: str) -> Optional[sqlite3.Connection]:
+    """
+    Connect to the SQLite database if it's exist or create a new one.
+    :param path: Path to the SQLite database
+    :return: Connection object or None if connection failed
+    """
+    connection: Optional[sqlite3.Connection] = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+
+def execute_query(connection: sqlite3.Connection, query: str) -> None:
+    """
+    Execute a query on the SQLite database.
+    :param connection: Connection to the SQLite database
+    :param query: SQL query to execute
+    """
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+def open_query_file(path: str) -> str:
+    """
+    Open a file with SQL query and return the query as a string.
+    :param path: Path to the file with SQL query
+    :return: SQL query as a string
+    """
+    with open(path, "r") as f:
+        query = f.read().strip()
+        return query
+
+
+create_tours_table = open_query_file("services/create_tours_table.sql")
+create_tours = open_query_file("services/create_tours.sql")
+
+connection = create_connection("tours.sqlite")
+execute_query(connection=connection, query=create_tours_table)
+execute_query(connection=connection, query=create_tours)
---
+++ services/create_tours.sql
@@ -0,0 +1,29 @@
+INSERT INTO tours (is_group, have_children, kids, visit, tours_list)
+VALUES (
+        'individual',
+        'adults',
+        NULL,
+        'lot',
+        '["vr", "liteiny", "myths", "liter", "informal", "kamenny", "vasil", "sightseeng_gr", "blockade", "sightseeng_kid", "kolomna", "petrogr", "fortress_priv", "hermit_kid", "rus_mus_kid", "sightseeng_priv", "fortr_kid_q", "secrets_kid_q"]'
+    ),
+    (
+        'group',
+        'kids',
+        'toddler',
+        'first',
+        '["vr", "liteiny", "myths", "liter", "informal", "kamenny", "vasil", "sightseeng_gr", "blockade", "sightseeng_kid", "kolomna", "petrogr", "fortress_priv", "hermit_kid", "rus_mus_kid", "sightseeng_priv", "fortr_kid_q", "secrets_kid_q"]'
+    ),
+    (
+        'individual',
+        'kids',
+        'teen',
+        'local',
+        '["vr", "liteiny", "myths", "liter", "informal", "kamenny", "vasil", "sightseeng_gr", "blockade", "sightseeng_kid", "kolomna", "petrogr", "fortress_priv", "hermit_kid", "rus_mus_kid", "sightseeng_priv", "fortr_kid_q", "secrets_kid_q"]'
+    ),
+    (
+        'group',
+        'adults',
+        NULL,
+        'first',
+        '["vr", "liteiny", "myths", "liter", "informal", "kamenny", "vasil", "sightseeng_gr", "blockade", "sightseeng_kid", "kolomna", "petrogr", "fortress_priv", "hermit_kid", "rus_mus_kid", "sightseeng_priv", "fortr_kid_q", "secrets_kid_q"]'
+    );
